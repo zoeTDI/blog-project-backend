23e3a268cf9d3c797c9474134fb24d4f9cf095bc
Caldm <1832400547@qq.com>
2026-08-03 00:08:16 +0800

    style(ResultCodeEnum): 格式化代码

--- src/main/java/cn/caldm/www/common/domain/ResultCodeEnum.java
+++ src/main/java/cn/caldm/www/common/domain/ResultCodeEnum.java
@@ -4,7 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum ResultCodeEnum {
-    SUCCESS(200, "操作成功"),BAD_REQUEST(400, "请求参数错误"),
+    SUCCESS(200, "操作成功"),
+    BAD_REQUEST(400, "请求参数错误"),
     UNAUTHORIZED(401, "暂未登录或Token已过期"),
     FORBIDDEN(403, "没有相关权限"),
     INTERNAL_SERVER_ERROR(500, "系统内部异常");
