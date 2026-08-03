d69bb656de30c324fa1544a033cd14ac60655828
Caldm <1832400547@qq.com>
2026-08-03 13:03:52 +0800

    feat(login): 添加登录返回的dto类并更新测试接口代码

---
+++ src/main/java/cn/caldm/www/auth/jwt/dto/LoginResDTO.java
@@ -0,0 +1,29 @@
+package cn.caldm.www.auth.jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+/**
+ *
+ * 登录请求返回的信息
+ *
+ * @author caldm
+ */
+@Slf4j
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResDTO {
+    private long id;
+    private String email;
+    private String username;
+    private String nickname;
+    private String avatar;
+    private String token;
+    private String role;
+    private List<String> menus;
+}
--- src/main/java/cn/caldm/www/dev/controller/JwtTestController.java
+++ src/main/java/cn/caldm/www/dev/controller/JwtTestController.java
@@ -1,5 +1,6 @@
 package cn.caldm.www.dev.controller;
 
+import cn.caldm.www.auth.jwt.dto.LoginResDTO;
 import cn.caldm.www.auth.jwt.utils.JwtUtils;
 import cn.caldm.www.common.domain.Result;
 import cn.caldm.www.common.domain.ResultCodeEnum;
@@ -11,8 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  *
@@ -28,7 +28,7 @@ public class JwtTestController {
     static {
         SysUser user1 = new SysUser();
         user1.setId(1L);
-        user1.setUsername("张三");
+        user1.setUsername("admin@caldm.cn");
         user1.setPassword("123456");
         userMap.put(1, user1);
         SysUser user2 = new SysUser();
@@ -39,14 +39,25 @@ public class JwtTestController {
     }
 
     @RequestMapping("/login")
-    public Result<String> login(@RequestBody SysUser user) {
+    public Result<LoginResDTO> login(@RequestBody SysUser user) {
+
         for (SysUser dbUser: userMap.values()) {
             if (dbUser.getUsername().equals(user.getUsername())
                     && dbUser.getPassword().equals(user.getPassword())) {
                 LogUtils.info("登录成功！生成token！");
                 user.setId(dbUser.getId());
                 String token = JwtUtils.createToken(user);
-                return Result.success(token);
+                LoginResDTO dto = new LoginResDTO();
+                dto.setId(dbUser.getId());
+                dto.setEmail("example@email.com");
+                dto.setUsername(dbUser.getUsername());
+                dto.setNickname("Admin");
+                dto.setToken(token);
+                dto.setRole("暂未实现该功能");
+                List<String> menus = new ArrayList<>();
+                menus.add("暂未实现该功能");
+                dto.setMenus(menus);
+                return Result.success(dto);
             }
         }
         return Result.error(ResultCodeEnum.BAD_REQUEST);
