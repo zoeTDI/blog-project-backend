21dfd6b332764176f463e16a7440fa9843b6da38
Caldm <1832400547@qq.com>
2026-08-03 23:37:35 +0800

    feat(auth_context): 添加 UserAssembler
    - toDomain: 将数据库持久化对象 (PO) 转换为领域层实体 (SysUser)

---
+++ src/main/java/cn/caldm/www/auth_context/interfaces/assembler/UserAssembler.java
@@ -0,0 +1,38 @@
+package cn.caldm.www.auth_context.interfaces.assembler;
+
+import cn.caldm.www.auth_context.domain.model.SysUser;
+import cn.caldm.www.auth_context.infrastructure.persistence.po.SysUserPO;
+
+/**
+ *
+ *
+ *
+ * @author caldm
+ */
+public class UserAssembler {
+    /**
+     * 将数据库持久化对象 (PO) 转换为领域层实体 (SysUser)
+     */
+    public static SysUser toDomain(SysUserPO po) {
+        if (po == null) {
+            return null;
+        }
+        SysUser domainUser = new SysUser();
+        domainUser.setId(po.getId());
+        domainUser.setUsername(po.getUsername());
+        domainUser.setPassword(po.getPassword());
+        domainUser.setNickname(po.getNickname());
+        domainUser.setEmail(po.getEmail());
+        domainUser.setSalt(po.getSalt());
+        domainUser.setAvatar(po.getAvatar());
+        domainUser.setStatus(po.getStatus());
+        domainUser.setLoginIp(po.getLoginIp());
+        domainUser.setLoginDate(po.getLoginDate());
+        domainUser.setCreator(po.getCreator());
+        domainUser.setCreateTime(po.getCreateTime());
+        domainUser.setUpdater(po.getUpdater());
+        domainUser.setUpdateTime(po.getUpdateTime());
+        domainUser.setDeleted(po.getDeleted());
+        return domainUser;
+    }
+}
