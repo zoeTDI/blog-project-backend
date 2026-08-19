87e4999321dd7ca3c55bce739a824ad0689bbb55
Caldm <1832400547@qq.com>
2026-08-20 06:17:06 +0800

    feat(post): 添加创建标签接口

--- src/main/java/cn/caldm/www/post_context/interfaces/rest/BlogPostTagController.java
+++ src/main/java/cn/caldm/www/post_context/interfaces/rest/BlogPostTagController.java
@@ -1,7 +1,15 @@
 package cn.caldm.www.post_context.interfaces.rest;
 
+import cn.caldm.www.common.domain.Result;
+import cn.caldm.www.common.domain.ResultCodeEnum;
+import cn.caldm.www.post_context.application.service.BlogPostTagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 /**
  *
@@ -12,5 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/post")
 public class BlogPostTagController {
+    @Autowired
+    private BlogPostTagService tagService;
 
+    @GetMapping("/createTag")
+    public Result createTag(@RequestParam("tagName") String tagName) {
+        if (tagName == null || tagName.isBlank()) {
+            return Result.error(ResultCodeEnum.BAD_REQUEST);
+        }
+        Long tagId = tagService.createTag(tagName);
+        if (tagId == null) {
+            return Result.error(ResultCodeEnum.INTERNAL_SERVER_ERROR);
+        }
+        return Result.success(Collections.singletonMap("tagId", tagId));
+    }
 }
