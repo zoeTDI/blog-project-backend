2fc9ba4254cbae61172545194251a9ce0920219f
Caldm <1832400547@qq.com>
2026-08-31 05:02:45 +0800

    feat(mapper): 为文章-标签关联添加批量插入方法

--- src/main/java/cn/caldm/www/post_context/infrastructure/persistence/mapper/BlogPostCategoryRelationMapper.java
+++ src/main/java/cn/caldm/www/post_context/infrastructure/persistence/mapper/BlogPostCategoryRelationMapper.java
@@ -3,7 +3,16 @@ package cn.caldm.www.post_context.infrastructure.persistence.mapper;
 import cn.caldm.www.post_context.infrastructure.persistence.po.BlogPostCategoryRelationPO;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface BlogPostCategoryRelationMapper extends BaseMapper<BlogPostCategoryRelationPO> {
+    /**
+     * 批量插入关联记录
+     * @param list 关联PO列表
+     * @return 插入条数
+     */
+    int insertBatch(@Param("list") List<BlogPostCategoryRelationPO> list);
 }
