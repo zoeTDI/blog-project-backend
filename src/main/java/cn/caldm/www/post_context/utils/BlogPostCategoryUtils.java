29f113c757cf820a189dd27b592abc65a3f1131c
Caldm <1832400547@qq.com>
2026-09-01 20:08:03 +0800

    feat(post/utils): 添加分类树的构建与扁平化静态方法

--- src/main/java/cn/caldm/www/post_context/utils/BlogPostCategoryUtils.java
+++ src/main/java/cn/caldm/www/post_context/utils/BlogPostCategoryUtils.java
@@ -14,30 +14,28 @@ import cn.caldm.www.post_context.domain.model.CategoryTreeNode;
 public class BlogPostCategoryUtils {
     /**
      * 将扁平的分类列表构建为属性结构，并返回所有根节点
-     * 
+     *
      * @param categoryList 分类列表
      * @return 根节点列表
      */
     public static List<CategoryTreeNode> buildTree(List<BlogPostCategory> categoryList) {
         Map<Long, CategoryTreeNode> map = new HashMap<>();
-        categoryList.stream()
-                .forEach(category -> {
-                    Long id = category.getId();
-                    CategoryTreeNode node = new CategoryTreeNode()
-                            .setCategory(category)
-                            .setChildren(new ArrayList<>());
-                    map.put(id, node);
-                });
-        categoryList.stream()
-                .forEach(category -> {
-                    Long id = category.getId();
-                    Long parentId = category.getParentId();
-                    CategoryTreeNode parentNode = map.get(parentId);
-                    if (parentNode == null) {
-                        return;
-                    }
-                    parentNode.getChildren().add(map.get(id));
-                });
+        categoryList.forEach(category -> {
+            Long id = category.getId();
+            CategoryTreeNode node = new CategoryTreeNode()
+                    .setCategory(category)
+                    .setChildren(new ArrayList<>());
+            map.put(id, node);
+        });
+        categoryList.forEach(category -> {
+            Long id = category.getId();
+            Long parentId = category.getParentId();
+            CategoryTreeNode parentNode = map.get(parentId);
+            if (parentNode == null) {
+                return;
+            }
+            parentNode.getChildren().add(map.get(id));
+        });
         return map.values()
                 .stream()
                 .filter(node -> node.getCategory().getParentId() == 0L)
@@ -46,7 +44,7 @@ public class BlogPostCategoryUtils {
 
     /**
      * 将树形结构的分类节点扁平化
-     * 
+     *
      * @param rootNodes 根节点列表
      * @return 所有节点对应的分类，若输入null或空则返回空列表
      */
