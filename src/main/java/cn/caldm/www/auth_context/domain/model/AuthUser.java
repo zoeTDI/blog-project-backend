11e9505e32116ca7da9ef54538024efd12c6a0a2
Caldm <1832400547@qq.com>
2026-09-12 20:19:43 +0800

    feat: AuthUser适配UserDetails

--- src/main/java/cn/caldm/www/auth_context/domain/model/AuthUser.java
+++ src/main/java/cn/caldm/www/auth_context/domain/model/AuthUser.java
@@ -4,8 +4,14 @@ import cn.caldm.www.user_context.domain.modal.RoleEnum;
 import cn.caldm.www.user_context.domain.modal.SysUserDeletedEnum;
 import cn.caldm.www.user_context.domain.modal.SysUserStatusEnum;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  *
@@ -14,7 +20,7 @@ import java.util.List;
  * @author caldm
  */
 @Data
-public class AuthUser {
+public class AuthUser implements UserDetails {
     private Long id;
     private String email;
     private String username;
@@ -29,4 +35,43 @@ public class AuthUser {
     private String refreshToken;
     private Long accessTokenExpiresAt;
     private Long refreshTokenExpiresAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+
+        if (roles != null) {
+            for (RoleEnum role : roles) {
+                authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getCode()));
+            }
+        }
+
+        if (menus != null) {
+            for (String menu : menus) {
+                authorities.add(new SimpleGrantedAuthority(menu));
+            }
+        }
+
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !SysUserStatusEnum.DISABLED.equals(this.status);
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return !SysUserDeletedEnum.DELETED.equals(this.deleted);
+    }
 }
