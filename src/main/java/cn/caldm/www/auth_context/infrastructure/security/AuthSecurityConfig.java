98871bbdceda0d9cc4881cc7acbf79dfb9fc2c76
Caldm <1832400547@qq.com>
2026-09-12 20:47:46 +0800

    feat: 开启方法级安全

--- src/main/java/cn/caldm/www/auth_context/infrastructure/security/AuthSecurityConfig.java
+++ src/main/java/cn/caldm/www/auth_context/infrastructure/security/AuthSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -25,6 +26,7 @@ import java.io.IOException;
  */
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class AuthSecurityConfig {
 
     @Bean
