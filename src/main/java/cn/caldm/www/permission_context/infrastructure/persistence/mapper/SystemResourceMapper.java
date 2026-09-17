f111db85e22f977d3f73e9a4efe2ddf78515efca
Caldm <1832400547@qq.com>
2026-09-17 19:48:52 +0800

    feat: 添加 SystemResourceMapper

---
+++ src/main/java/cn/caldm/www/permission_context/infrastructure/persistence/mapper/SystemResourceMapper.java
@@ -0,0 +1,9 @@
+package cn.caldm.www.permission_context.infrastructure.persistence.mapper;
+
+import cn.caldm.www.permission_context.infrastructure.persistence.po.SystemResourcePO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SystemResourceMapper extends BaseMapper<SystemResourcePO> {
+}
