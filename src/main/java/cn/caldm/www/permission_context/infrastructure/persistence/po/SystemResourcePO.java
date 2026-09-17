663ed3faa4a193635dd4a5547a829f4da07d1f0d
Caldm <1832400547@qq.com>
2026-09-17 19:45:48 +0800

    feat: SystemResourcePO 添加更多字段

--- src/main/java/cn/caldm/www/permission_context/infrastructure/persistence/po/SystemResourcePO.java
+++ src/main/java/cn/caldm/www/permission_context/infrastructure/persistence/po/SystemResourcePO.java
@@ -1,15 +1,21 @@
 package cn.caldm.www.permission_context.infrastructure.persistence.po;
 
+import cn.caldm.www.permission_context.domain.model.ResourceDeletedEnum;
+import cn.caldm.www.permission_context.domain.model.ResourceStatusEnum;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 
 import cn.caldm.www.permission_context.domain.model.ResourceTypeEnum;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @TableName("system_menu")
 public class SystemResourcePO {
+
     @TableId(value = "id", type = IdType.AUTO)
     private Long id;
     private String name;
@@ -22,4 +28,10 @@ public class SystemResourcePO {
     private String icon;
     private String titleKey;
     private ResourceStatusEnum status;
+    private String creator;
+    private LocalDateTime createTime;
+    private String updater;
+    private LocalDateTime updateTime;
+    @TableLogic
+    private ResourceDeletedEnum deleted;
 }
